25a605a61feb7771a46f4bb6cbedd0178b1f29e5
j-tyler <justin.marsh@holbertonschool.com>
2016-08-23 00:31:44 +0000

    Exercise 2-4 from K&R - rough

--- TheCProgrammingLanguage/go-squeezestring-e2p4.go
+++ TheCProgrammingLanguage/go-squeezestring-e2p4.go
@@ -2,24 +2,30 @@ package main
 
 import "fmt"
 
-// Tried multiple implimentations, none have worked.
-// not quite sure how GO strings work yet :/
+// Labeling rough because I don't quite understand slices
+// not sure how to change s1 without an assignment to the fuction return
+// but this could be proper implementation.
+
+
+// When squeeze is given two arrays, remove all characters found in string2
+// from string1.
+
+func squeeze(s1 string, s2 string) string {
+	for s1r, s2r, i := []rune(s1), []rune(s2), 0; i < len(s1r); i++ {
+        for j := 0; j < len(s2r); j++ {
+            if s1r[i] == s2r[j] {
+                s1r = s1r[:i+copy(s1r[i:], s1r[i+1:])]  // I don't understand how this
+            }                                           // works, got from golang wiki
+        }
+        s1 = string(s1r)
+    }
+	return s1
+}
 
 func main() {
 	var s1 string = "Hello, mj. Does this program work?"
 	var s2 string = "ek"
-	
-	for s3, i := "", 0; i < len(s1); i++ {
-		fmt.Println(i)
-		for samechar, j, i := false, 0, i; j < len(s2); j++ {
-			if s1[i] == s2[j] {
-				samechar = true
-			}
-			if samechar == false && j+1 == len(s2) {
-				s3 += string(s1[i])
-			}
-		}
-		s1 = s3
-	}
-	fmt.Println(s1, s2)
+
+	s1 = squeeze(s1, s2)
+	fmt.Println(s1)
 }
