b4df6cd2b023ca52399c27d60f05632725d2939c
j-tyler <justin.marsh@holbertonschool.com>
2017-03-07 03:54:53 +0000

    Exercise1-7.go

---
+++ TheGoProgrammingLanguage/Exercise1-7.go
@@ -0,0 +1,41 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// See page 16.
+// Edited to solve Exercise 1.7 in The Go Programming Language
+// by Justin T. Marsh.
+//!+
+
+// Fetch prints the content found at each specified URL.
+
+// Exercise 1.7: The function call io.Copy(dst, src) reads from src and
+// writes to dst. Use it instead of ioutil.ReadAll to copy the response
+// body to oi.Stdout without requiring a buffer large enough to hold the
+// entire stream. Be sure to check the error result of io.Copy.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+//!-
